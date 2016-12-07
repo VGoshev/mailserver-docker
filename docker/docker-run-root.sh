bf15e635940b1856e79ab537d7eafe6f229389cd
Vladimir A. Goshev (sunx.me) <sunx_git@sunx.name>
2016-12-07 21:36:53 +0100

    Remove pid files on start

--- docker/docker-run-root.sh
+++ docker/docker-run-root.sh
@@ -5,6 +5,7 @@ ROOT_DIR="/home/mail"
 
 [ -d "$ROOT_DIR/var" ] || tar xf /etc/config.tar -C $ROOT_DIR
 
+rm -f /run/dovecot/master.pid /run/exim.pid /run/rspamd/rspamd.pid
 
 /usr/sbin/rspamd -u rspamd -g rspamd -c /etc/rspamd/rspamd.conf
 
