c5464ecf1a23fcf18a00aebfb55addc4b4621d59
Vladimir A. Goshev (sunx.me) <sunx_git@sunx.name>
2016-12-11 21:35:13 +0100

    Add ability to set ports fo docker-run.sh script

--- docker-run.sh
+++ docker-run.sh
@@ -15,6 +15,7 @@ EXTRA_ARGS="-d -ti"
 DOCKER="docker"
 #Extra args to docker command. Like using remote dockerd or something else
 DOCKER_ARGS=""
+USED_PORTS="-p 993:993 -p 995:995 -p 110:110 -p 143:143 -p 25:25 -p 465:465"
 
 #You can change default values by adding them to config file
 CONFIG_FILE="~/.docker-sunx-mailserver"
@@ -25,13 +26,10 @@ CONFIG_FILE=`eval echo $CONFIG_FILE`
 [ ! -z "$CONTAINER_NAME" ]     && CONTAINER_NAME="--name=$CONTAINER_NAME"
 [ ! -z "$RESTART_POLICY" ]     && RESTART_POLICY="--restart=$RESTART_POLICY"
 
-# Do you need POP3 protocol?
 
 $DOCKER $DOCKER_ARGS run \
 	-v $VOLUME_PATH:/home/mail \
-	-p 993:993 -p 995:995 \
-	-p 110:110 -p 143:143 \
-	-p 25:25 -p 465:465 \
+	$USED_PORTS \
 	$CONTAINER_HOSTNAME \
 	$CONTAINER_NAME \
 	$RESTART_POLICY \
