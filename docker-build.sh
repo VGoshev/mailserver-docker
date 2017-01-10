6b007c0a23188ee0ec9261cfc9accd572083b755
Vladimir A. Goshev (Akari) <sunx_git@sunx.name>
2017-01-10 11:16:50 +0300

    Add support of building images for armhf (Raspberry Pi, etc...) architecture

--- docker-build.sh
+++ docker-build.sh
@@ -2,4 +2,25 @@
 
 cd `dirname $0`
 
-docker build -t sunx/mailserver ./docker/
+ARCH=`uname -m`
+IMAGE="sunx/mailserver"
+
+if `echo $ARCH | grep -q arm`; then
+	echo arm
+	rm -rf ./build
+	cp -a ./docker ./build
+
+	#Use armhf/alpine as base image instead of alpine
+	sed -i -r 's,(FROM)\s+(alpine),\1 armhf/\2,' build/Dockerfile
+
+	docker build -t $IMAGE ./build/
+	#&& rm -rf ./build
+else
+	if `echo $ARCH | grep -q x86_64`; then
+		docker build -t $IMAGE ./docker/
+	else 
+		echo "Error: Architecture $ARCH isn't supported"
+		exit 1
+	fi
+fi
+
